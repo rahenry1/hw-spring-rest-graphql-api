c74fb6784138641153c56a67077003d20a6822cb
rahenry1 <rajadurai.henry@gmail.com>
2023-05-14 18:05:58 -0500

    dev

---
+++ src/main/java/com/hw/api/hwspringrestgraphqlapi/HwSpringRestGraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.hw.api.hwspringrestgraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringRestGraphqlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpringRestGraphqlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hw/api/hwspringrestgraphqlapi/HwSpringRestGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.api.hwspringrestgraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringRestGraphqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
